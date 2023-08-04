13da7f0c57e2e1cb1e5d29725bab3e0aa5c29307
Michael Sproul <michael@sigmaprime.io>
2023-08-04 13:39:36 +1000

    Simplify Decode error handling

--- src/Decode.hs
+++ src/Decode.hs
@@ -3,25 +3,20 @@ module Decode where
 import qualified Data.ByteString.Lazy as BS
 import Data.Binary.Get (Get, getWord32le, getLazyByteString, getRemainingLazyByteString, lookAhead, runGetOrFail, isEmpty)
 import Data.Int (Int64)
-import Control.Monad (forM, foldM)
+import Control.Monad (forM, foldM, when)
 import Data.Maybe (fromMaybe)
 
 import Common (Ssz, U32, U64, bytesPerLengthOffset, pairs)
 
-data DecodeError = InvalidBytes String |
-    TooManyItems
-    deriving (Show)
+type DecodeError = String
 
 class (Ssz a) => Decode a where
     sszDecode :: BS.ByteString -> Either DecodeError a
 
 decodeOrError :: Get a -> BS.ByteString -> Either DecodeError a
 decodeOrError parser bytes = case runGetOrFail parser bytes of
-    Left (_, _, err) -> Left (InvalidBytes err)
+    Left (_, _, err) -> Left err
     Right (_, _, val) -> Right val
-
-readOffset :: BS.ByteString -> Either DecodeError U32
-readOffset = decodeOrError getOffset
 
 getOffset :: Get U32
 getOffset = getWord32le
@@ -41,24 +36,21 @@ offsetDiff [x] = Nothing
 offsetDiff [x, y] = Just (fromIntegral (y - x))
 offsetDiff _ = Nothing
 
--- TODO: reconsider error handling (use of fail)
 getListOfVariableLengthItems :: (Traversable t, Applicative t, Monoid (t a), Decode a) => Maybe U32 -> Get (t a)
 getListOfVariableLengthItems maxItems = do
     empty <- isEmpty
-    if empty
-    then return mempty
+    if empty then
+        return mempty
     else do
         firstOffset <- lookAhead getOffset
-        if firstOffset `mod` fromIntegral bytesPerLengthOffset /= 0 || firstOffset == 0 then
+        when (firstOffset `mod` fromIntegral bytesPerLengthOffset /= 0 || firstOffset == 0) $
             fail $ "invalid first offset: " ++ show firstOffset
-        else do
-            let numItems = firstOffset `div` fromIntegral bytesPerLengthOffset
-            if fromMaybe False (fmap (numItems>) maxItems)
-            then fail (show TooManyItems)
-            else do
-                offsets :: [U32] <- forM [1..numItems] (\_ -> getOffset)
-                let lengths = map offsetDiff (pairs offsets)
-                items <- foldM (\xs maybeLen -> do
-                    x <- getSszItem maybeLen
-                    return (mappend xs (pure x))) mempty lengths
-                return items
+        let numItems = firstOffset `div` fromIntegral bytesPerLengthOffset
+        when (fromMaybe False (fmap (numItems>) maxItems)) $
+            fail $ "too many items: " ++ show numItems
+        offsets <- forM [1..numItems] (const getOffset)
+        let lengths = map offsetDiff (pairs offsets)
+        items <- foldM (\xs maybeLen -> do
+            x <- getSszItem maybeLen
+            return (mappend xs (pure x))) mempty lengths
+        return items
